0629482d9c79fece86e8b2ec7a656c42497a0810
ibraimov07 <serveribraimov7@proton.me>
2026-02-06 19:08:43 +0300

    chat and full

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,21 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+const wa = window.Telegram.WebApp;
+const mobilePlatforms = ["ios", "android"];
+const isMobile = mobilePlatforms.includes(wa.platform);
+if (isMobile) {
+  wa.requestFullscreen();
+  wa.disableVerticalSwipes();
+  wa.expand();
+}
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <div className={!isMobile ? "is_desktop" : "is_tma"}>
+      <App />
+    </div>
   </StrictMode>,
 )
 
